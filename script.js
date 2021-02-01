595ca2c214c2416251b402f25278a2d0ab6015e1
jasminelychan <77300672+jasminelychan@users.noreply.github.com>
2021-02-01 03:28:46 -0500

    edit script, put in ready

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ console.log("project 3")
 $(document).ready(function(){
     $(".item").draggable({snap:true});
 
-
+    $(".item").mouseover(function(){
+        $(".item").css("color","green");    
+    });
+    
+    $(".item").each(function(){
+        $(".item").css("color","green");    
+    });
+    
+    $(".item").mouseout(function(){
+        $(".item").css("color","gray");    
+    });
 
 });
 
 
 
-$(".item").mouseover(function(){
-    $(".item").css("color","green");    
-});
 
-$(".item").each(function(){
-    $(".item").css("color","green");    
-});
-
-$(".item").mouseout(function(){
-    $(".item").css("color","gray");    
-});
 
 
 
